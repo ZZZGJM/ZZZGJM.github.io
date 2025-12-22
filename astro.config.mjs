99862bef5b51cff5f0995f50ce35873691044d70
Kamio-Misuzu <1176017541@qq.com>
2025-12-22 23:37:28 +0800

    Fix GitHub Pages deployment: update site URL and branch name

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://Kamio-Misuzu.github.io",
+	site: "https://ZZZGJM.github.io",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
